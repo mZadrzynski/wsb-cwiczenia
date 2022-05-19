ea5fffb17488a4c32537d86355af1f3863485d54
Marcin Zadrzynski <warshipdesign@gmail.com>
2022-05-19 13:43:09 +0200

    WSB progresss 18.05

--- src/com/company/Human.java
+++ src/com/company/Human.java
@@ -1,9 +1,33 @@
 package com.company;
+import java.util.Date;
+
+import org.w3c.dom.ls.LSOutput;
 
 public class Human {
     public String firstName;
     public String lastName;
-
     public Phone mobile;
     public Animal pet;
+    public Car car;
+    private Double salary;
+    public Date nowDate = new Date();
+
+
+    double getSalary(){
+        System.out.println("salary: ");
+        System.out.println(nowDate);
+        return this.salary;
+    }
+
+    void setSalary(double salary){
+        if(salary >= 0) {
+            this.salary = salary;
+            System.out.println("informacje zostały wysłane do systemu ksiegowego");
+            System.out.println("konieczności odebrania aneksu do umowy od pani Hani z kadr");
+            System.out.println("ZUS i US już wiedzą o zmianie wypłaty i nie ma sensu ukrywać dochodu");
+        } else {
+            System.out.println("jak to jestes niewolnikiem?");
+        }
+
+    }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,24 +1,34 @@
 package com.company;
 
+import java.util.Date;
+
 public class Main {
 
     public static void main(String[] args) {
        Human marcin = new Human();
        Car karlos = new Car("corolla","toyota");
-
-
        Animal Ares = new Animal("dog", 2.0);
+       marcin.car = karlos;
+       marcin.pet = Ares;
 
+
+
+        marcin.setSalary(2000);
+        System.out.println(marcin.getSalary());
+
+
+        /*
        System.out.println(Ares.weight);
        Ares.feed();
        Ares.takeForAWalk();
        System.out.println(Ares.weight);
        Ares.takeForAWalk();
        System.out.println(Ares.weight);
-        Ares.takeForAWalk();
-        Ares.takeForAWalk();
-        Ares.takeForAWalk();
-        System.out.println(Ares.weight);
+       Ares.takeForAWalk();
+       Ares.takeForAWalk();
+       Ares.takeForAWalk();
+       System.out.println(Ares.weight);
+       */
 
     }
 }
